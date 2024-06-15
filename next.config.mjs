1c4125a8053d3814c6b05d51836e0a034b0f37bb
yabuki829 <syodai829@icloud.com>
2024-06-15 10:25:10 +0900

    cloudnayの設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "res.cloudinary.com",
+          pathname: "/**",
+        },
+      ],
+    },
+    experimental: {
+      serverActions: {
+        bodySizeLimit: "2mb",
+      },
+    },
+  }
+  
 export default nextConfig;
 
