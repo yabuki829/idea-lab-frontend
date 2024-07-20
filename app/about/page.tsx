9db82d2583ec46f6cc502cfb2e6462649245b60f
yabuki829 <syodai829@icloud.com>
2024-07-20 22:13:23 +0900

    aboutページUI修正

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 
 import AnimationText from "@/components/ui/animationText";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link"
 const Home = () => {
   return (
     <div className=" bg-gradient-to-t from-indigo-100 px-5 md:px-10 ">
@@ -14,21 +14,81 @@ const Home = () => {
           
         </div>
         <h1 className="font-bold text-4xl"></h1>
-        <div className="md:flex justify-between">
+        <div className="">
+           <div className="flex justify-center ">
            <div className="mx-2">
-           <Image src="/top-1.png" alt="top-1" width={500} height={300} />
-            <p className="text-xl text-center  border-l-2 border-b-2 rounded-l-full ">1. アイデアを生成</p>
-          </div>
-          <div className="mx-2">
-            <Image src="/top-2.png" alt="top-2" width={500} height={300} />
-            <p className="text-xl text-center  border-l-2 border-b-2 rounded-l-full">2. アイデアを共有</p>
-          </div>
-          <div className="mx-2">
-            <Image src="/top-3.png" alt="top-3" width={500} height={300} />
-            <p className="text-xl text-center border-l-2 border-b-2 rounded-l-full">3. アイデアを磨く</p>
-          </div>
+            <Image src="/post.png" alt="top-1" width={500} height={300} />
+            <div className="block md:hidden text-center">
+              <p className="text-2xl  font-bold ">AIがアイデアの生成をお手伝い</p>
+              <p>話題のChatGPTがあなたのアイデアの生成をお手伝いします。</p>
+              <p>好きな単語を入力し、自動生成を押すだけで簡単アイデア生成</p>
+             </div>
+            
+            </div>
+            <div className="flex-col  justify-center items-center w-1/2 items-center bg-yellow-300 rounded-full m-10 hidden md:flex">
+              <h1 className="font-bold text-sm md:text-3xl text-center ">AIがアイデアの生成をお手伝い</h1>
+              <div className="mx-auto w-2/3">
+                <p>話題のChatGPTがあなたのアイデアの生成をお手伝いします。</p>
+
+                <p>好きな単語を入力し、自動生成を押すだけで簡単アイデア生成</p>
+              </div>
+              <br />
+              <Link className="bg-blue-300 px-10 py-2 rounded-md text-white" href="/login">ログイン</Link>
+            </div>
+           </div>
+    
+
+           <div className="flex justify-center my-20 ">
+           
+            <div className="flex-col  justify-center items-center w-1/2 items-center bg-blue-200 rounded-full mx-20 my-10 hidden md:flex">
+              <h1 className="font-bold text-sm md:text-3xl text-center ">みんなのアイデアを見よう</h1>
+              <div className="mx-auto w-2/3 text-center">
+                <p>みんなのアイデアを見て刺激を受けよう</p>
+
+                <p>他の人のアイデアを見ることで新しいアイデアが浮かぶかも</p>
+              </div>
+              <br />
+              <Link className="bg-orange-400 text-white px-10 py-2 rounded-md" href="/login">ログイン</Link>
+            </div>
+            
+            <div className="mx-2">
+            <Image src="/idea.png" alt="top-1" width={500} height={300} />
+            <div className="block md:hidden text-center">
+              <p className="text-2xl  font-bold ">みんなのアイデアを見よう</p>
+              <p>みんなのアイデアを見て刺激を受けよう</p>
+              <p>他の人のアイデアを見ることで新しいアイデアが浮かぶかも</p>
+             </div>
+            
+            </div>
+           </div>
+          <br />
+
+
+          <div className="flex justify-center ">
+           <div className="mx-2">
+              <Image src="/monetize.png" alt="top-1" width={500} height={300} />
+             <div className="block md:hidden text-center">
+              <p className="text-2xl  font-bold ">AIがマネタイズの生成をお手伝い</p>
+              <p>ChatGPTがマネタイズを考えるのをお手伝い</p>
+              <p>ボタンを押すだけ簡単生成</p>
+             </div>
+            </div>
+            <div className="flex-col  justify-center items-center w-1/2 items-center bg-red-300 rounded-full mx-20 my-10 hidden md:flex">
+              <h1 className="font-bold text-sm md:text-3xl text-center ">AIがマネタイズの生成をお手伝い</h1>
+              <div className="mx-auto w-2/3 text-center">
+                <p>ChatGPTがマネタイズを考えるのをお手伝い</p>
+
+                <p>ボタンを押すだけ簡単生成</p>
+                
+              </div>
+              <br />
+              <Link className="bg-green-200 px-10 py-2 rounded-md" href="/login">ログイン</Link>
+            </div>
+           </div>
         </div>
       </div>
+
+      
       <br />
       <div>
         <div>
